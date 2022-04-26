b422f3cb2d282c4d2f3419fa0af0c762217081e8
SaitoYuki <saito@ChickenFilet-O.local>
2022-04-26 15:08:04 +0900

    first commit

---
+++ splash-screens-test/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  splash-screens-test
+//
+//  Created by 齋藤有希 on 2022/04/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ splash-screens-test/splash_screens_testApp.swift
@@ -0,0 +1,17 @@
+//
+//  splash_screens_testApp.swift
+//  splash-screens-test
+//
+//  Created by 齋藤有希 on 2022/04/26.
+//
+
+import SwiftUI
+
+@main
+struct splash_screens_testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
